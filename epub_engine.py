1ff0be4bcc1be5ea01905d608938b1896e555193
Dmitry Reznikov <Ontaelio@users.noreply.github.com>
2024-01-20 02:03:03 +0400

    epub converter txt output

--- epub_engine.py
+++ epub_engine.py
@@ -10,6 +10,30 @@ def get_template(chapter: epub.EpubHtml) -> str:
     return res[0]
 
 
+def prepare_ebook(book: epub.EpubBook, template=None) -> epub.EpubBook:
+    new_book = epub.EpubBook()
+
+    new_book.metadata = book.metadata
+    new_book.spine = book.spine
+    new_book.guide = book.guide
+    new_book.toc = book.toc
+    new_book.bindings = book.bindings
+    new_book.pages = book.pages
+    new_book.title = book.title
+    new_book.language = book.language
+    new_book.direction = book.direction
+    new_book.templates = book.templates
+    if template:
+        new_book.templates['chapter'] = template  # default template is evil
+    new_book.prefixes = book.prefixes
+    new_book.namespaces = book.namespaces
+    new_book.IDENTIFIER_ID = book.IDENTIFIER_ID
+    new_book.FOLDER_NAME = book.FOLDER_NAME
+    new_book.EPUB_VERSION = book.EPUB_VERSION
+
+    return new_book
+
+
 def convert_chapter(chapter: epub.EpubHtml, options: dict) -> epub.EpubHtml:
     content = chapter.content.decode('utf-8')
     processed_text = ru_to_arm(content, options).encode('utf-8')
@@ -30,30 +54,11 @@ def convert_epub(book: epub.EpubBook, options: dict) -> epub.EpubBook:
         else:
             kept_items.append(_)
 
-    new_book = epub.EpubBook()
-
-    new_book.metadata = book.metadata
-    new_book.spine = book.spine
-    new_book.guide = book.guide
-    new_book.toc = book.toc
-    new_book.bindings = book.bindings
-    new_book.pages = book.pages
-    new_book.title = book.title
-    new_book.language = book.language
-    new_book.direction = book.direction
-    new_book.templates = book.templates
-    new_book.templates['chapter'] = template  # default template is evil
-    new_book.prefixes = book.prefixes
-    new_book.namespaces = book.namespaces
-    new_book.IDENTIFIER_ID = book.IDENTIFIER_ID
-    new_book.FOLDER_NAME = book.FOLDER_NAME
-    new_book.EPUB_VERSION = book.EPUB_VERSION
+    new_book = prepare_ebook(book, template)
 
     for _ in kept_items:
-        #new_book.add_item(_)
         new_book.items.append(_)
     for _ in content_items:
-        # new_book.add_item(_)
         new_book.items.append(_)
 
     return new_book
@@ -66,17 +71,16 @@ def epub2txt(book: epub.EpubBook) -> str:
     for _ in book_items:
         if type(_) == epub.EpubHtml:
             soup = BeautifulSoup(_.get_body_content(), 'html.parser')
-            text = [para.get_text() for para in soup.find_all('p')]
+            text = [' '.join(para.get_text().split()) for para in soup.find_all('p')]
             text_items.append('\n'.join(text))
 
-    return '\n\n\n\n\n'.join(text_items)
+    return '\n\n'.join(text_items)
 
 
 if __name__ == '__main__':
-    boo = epub.read_epub('texts/test.epub')
+    boo = epub.read_epub('texts/stross.epub')
     # (boo.templates)
     # print(dir(boo))
-    # print(epub2txt(boo))
+    print(epub2txt(boo))
     new_boo = convert_epub(boo, VOWELS)
-    epub.write_epub('texts/test_write.epub', new_boo)
-
+    epub.write_epub('texts/stross_write.epub', new_boo)
